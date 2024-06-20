31562ddb7cc58aef22d7b8aae0b767a71a6c7f26
sunao nakao <sunawo.m.lg@gmail.com>
2024-06-20 18:33:33 +0900

    Spring Security ログインエラーメッセージの書き換え

--- springboot-admin/src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ springboot-admin/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,18 +1,35 @@
 package com.example.demo.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.example.demo.constant.UrlConst;
 
+import lombok.RequiredArgsConstructor;
+
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig {
 	
+	/** パスワードエンコーダー **/
+	private final PasswordEncoder passwordEncoder;
 	
+	/** ユーザー情報取得Servise **/
+	private final UserDetailsService userDetailsService;
+	
+	/** メッセージ取得 **/
+	private final MessageSource messageSource;
+	
+	/** ユーザー名のname属性 **/
 	private final String USRANAME_PARAMETER = "email";
 	
 	
@@ -32,6 +49,21 @@ public class WebSecurityConfig {
 	
 		
 		return http.build();
+	}
+	
+	
+	/** Provider定義 ** 
+	 * ＠return カスタマイズProvider情報
+	 */
+	
+	@Bean
+	AuthenticationProvider daoAuthenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		provider.setPasswordEncoder(passwordEncoder);
+		provider.setMessageSource(messageSource);
+		
+		return provider;
 	}
 	
 
